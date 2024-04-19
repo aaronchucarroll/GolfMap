fcca9cae0e452aaf9a2e3e6eb1b247682a73984d
Aaron Chu-Carroll <aaron.chucarroll@gmail.com>
2024-04-19 14:28:45 -0400

    Initial Commit

---
+++ GolfMap/GolfMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfMapApp.swift
+//  GolfMap
+//
+//  Created by Aaron Chu-Carroll on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GolfMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
